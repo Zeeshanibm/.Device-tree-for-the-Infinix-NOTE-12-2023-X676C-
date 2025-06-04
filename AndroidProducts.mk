fb6d1058289186f10e93c851b182ff402d4514cf
Muhammad Zeeshan <48457235+Zeeshanibm@users.noreply.github.com>
2025-06-04 10:01:10 +0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,9 @@
 #
-# Copyright (C) 2024 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X676C.mk
+    $(LOCAL_DIR)/twrp_Infinix-X676C.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X676C-user \
-    lineage_X676C-userdebug \
-    lineage_X676C-eng
+    twrp_Infinix-X676C-eng
